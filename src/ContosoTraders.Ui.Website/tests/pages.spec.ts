b62cdd120e671778582f1893a72da14ce18149a0
vzisiadis <vzisiadis@gmail.com>
2024-02-01 14:58:29 +0200

    skip carousel tests and reenable stress tests

--- src/ContosoTraders.Ui.Website/tests/pages.spec.ts
+++ src/ContosoTraders.Ui.Website/tests/pages.spec.ts
@@ -61,7 +61,8 @@ test.describe('Carousel', () => {
   })
 });
 
-test.describe('CarouselVRT', () => {  
+test.describe.skip('CarouselVRT', () => {  
+  test.skip(({ browserName }) => browserName !== 'chromium', 'Chromium only!');
   test('verify carousel is pixel perfect - slide 1', async ({ page }) => {
     await expect(page.getByTestId('carousel')).toHaveScreenshot();
   })
